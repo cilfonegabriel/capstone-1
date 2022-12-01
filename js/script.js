bac51a0b5db1663c723ab017f8c317ded3a9f64b
gabicilfone <cilfonegabriel@gmail.com>
2022-12-01 17:15:41 -0300

    fix linter error

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
 window.onload = () => {
-    const guestSpeakers = [
-      {
-        image: './assets/images/square-issac.png',
-        name: 'Isaac Guerrero',
-        role: 'Technical Director',
-        bio: 'FC Barcelona Football School',
-      },
-      {
-        image: './assets/images/square-laporta.png',
-        name: 'Joan Laporta ',
-        role: 'President of the Barcelona club',
-        bio: 'Businessman, politician and lawyer.',
-      },
-      {
-        image: './assets/images/square-guardiola.png',
-        name: 'Josep Guardiola',
-        role: 'Former soccer player and soccer coach.',
-        bio: 'Manchester City',
-  
-      },
-      {
-        image: './assets/images/square-nevad.png',
-        name: 'Nenad Bjeica',
-        role: 'Football Coach',
-        bio: 'Gnk Dinamo Zabreg',
-      },
-  
-      {
-        image: './assets/images/square-joseph.png',
-        name: 'Johan Cruyff',
-        role: 'Former soccer player and soccer coach.',
-        bio: 'The best player in Europe of the 20th century',
-      },
-      {
-        image: './assets/images/bussines.png',
-        name: 'Daniela Alvez',
-        role: 'CEO of the technology department',
-        bio: 'Leader in Machine Learning and Barcelona systems.',
-      },
-    ];
-  
-    function displaySpeakerInfo(speakerInfo) {
-      const speakers = `<div class="speaker-1">
+  const guestSpeakers = [
+    {
+      image: './assets/images/square-issac.png',
+      name: 'Isaac Guerrero',
+      role: 'Technical Director',
+      bio: 'FC Barcelona Football School',
+    },
+    {
+      image: './assets/images/square-laporta.png',
+      name: 'Joan Laporta ',
+      role: 'President of the Barcelona club',
+      bio: 'Businessman, politician and lawyer.',
+    },
+    {
+      image: './assets/images/square-guardiola.png',
+      name: 'Josep Guardiola',
+      role: 'Former soccer player and soccer coach.',
+      bio: 'Manchester City',
+
+    },
+    {
+      image: './assets/images/square-nevad.png',
+      name: 'Nenad Bjeica',
+      role: 'Football Coach',
+      bio: 'Gnk Dinamo Zabreg',
+    },
+
+    {
+      image: './assets/images/square-joseph.png',
+      name: 'Johan Cruyff',
+      role: 'Former soccer player and soccer coach.',
+      bio: 'The best player in Europe of the 20th century',
+    },
+    {
+      image: './assets/images/bussines.png',
+      name: 'Daniela Alvez',
+      role: 'CEO of the technology department',
+      bio: 'Leader in Machine Learning and Barcelona systems.',
+    },
+  ];
+
+  function displaySpeakerInfo(speakerInfo) {
+    const speakers = `<div class="speaker-1">
       <div class="speaker-image">
           <img src=${speakerInfo.image} alt="speaker-1">
       </div>
@@ -51,40 +51,40 @@ window.onload = () => {
           <h4>${speakerInfo.bio}</h4>
       </div>
   </div>`;
-      return speakers;
-    }
-    const guestSpeakerAtEvent = document.querySelector('.image-display-1');
-    guestSpeakers.forEach((card) => {
-      guestSpeakerAtEvent.innerHTML += displaySpeakerInfo(card);
+    return speakers;
+  }
+  const guestSpeakerAtEvent = document.querySelector('.image-display-1');
+  guestSpeakers.forEach((card) => {
+    guestSpeakerAtEvent.innerHTML += displaySpeakerInfo(card);
+  });
+
+  const hamburger = document.querySelector('.menu');
+  const closeBtn = document.querySelector('.closeBtn');
+
+  hamburger.addEventListener('click', () => {
+    console.log('clicked');
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.display = 'block';
+  });
+
+  function close(menu, toBeClose) {
+    menu.addEventListener('click', () => {
+      const value = document.querySelector(toBeClose);
+      value.style.display = 'none';
     });
-  
-    const hamburger = document.querySelector('.menu');
-    const closeBtn = document.querySelector('.closeBtn');
-  
-    hamburger.addEventListener('click', () => {
-      console.log('clicked');
-      const menu = document.querySelector('.mobile-menu');
-      menu.style.display = 'block';
-    });
-  
-    function close(menu, toBeClose) {
-      menu.addEventListener('click', () => {
-        const value = document.querySelector(toBeClose);
-        value.style.display = 'none';
-      });
-    }
-  
-    close(closeBtn, '.mobile-menu');
-    closeBtn.addEventListener('click', () => {
-      const menu = document.querySelector('.mobile-menu');
-      menu.style.display = 'none';
-      close(menu);
-    });
-  
-    const backToMainPage = document.querySelector('.menuPopUp');
-    close(backToMainPage, '.mobile-menu');
-    backToMainPage.addEventListener('click', () => {
-      const menu = document.querySelector('.mobile-menu');
-      menu.style.display = 'none';
-    });
-  };
+  }
+
+  close(closeBtn, '.mobile-menu');
+  closeBtn.addEventListener('click', () => {
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.display = 'none';
+    close(menu);
+  });
+
+  const backToMainPage = document.querySelector('.menuPopUp');
+  close(backToMainPage, '.mobile-menu');
+  backToMainPage.addEventListener('click', () => {
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.display = 'none';
+  });
+};
